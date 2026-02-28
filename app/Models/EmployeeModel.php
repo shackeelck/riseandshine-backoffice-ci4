f1107477310f02c83dc72a23e61756429c07cdcb
Muhammed Shackeel Ck <shackeelck@gmail.com>
2026-02-28 22:43:36 +0500

    Initial commit

--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -8,7 +8,7 @@ class EmployeeModel extends Model
 {
     protected $table = 'employees';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['username','email',  'password', 'remember_token','role','status'];
+    protected $allowedFields = ['username','email',  'password', 'remember_token','role','status','api_token','api_token_created_at'];
     
     protected $useTimestamps = true;
     protected $createdField  = 'created_at';
