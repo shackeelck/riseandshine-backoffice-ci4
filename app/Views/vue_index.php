230105cda12cc48a94bc7279d48c8cc303fa61c4
Muhammed Shackeel Ck <shackeelck@gmail.com>
2026-06-12 11:30:17 +0500

    build copy

--- app/Views/vue_index.php
+++ app/Views/vue_index.php
@@ -5,7 +5,7 @@
     <link rel="icon" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rise And Shine Hotel</title>
-    <script type="module" crossorigin src="<?=base_url('/assets/index-IVuUVfLN.js');?>"></script>
+    <script type="module" crossorigin src="<?=base_url('/assets/index-BWUlLlHf.js');?>"></script>
     <link rel="stylesheet" crossorigin href="<?=base_url('/assets/index-BjUTw7q7.css')?>">
   </head>
   <body>
